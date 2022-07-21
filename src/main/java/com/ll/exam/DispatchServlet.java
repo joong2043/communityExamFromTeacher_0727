190d24162ef7858f7998f517fdcdfb284a7f2229
jhs512 <jangka512@gmail.com>
2022-07-21 15:29:37 +0900

    라우팅 기능 구현

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import com.ll.exam.article.ArticleController;
+import com.ll.exam.member.MemberController;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,7 +14,24 @@ import java.io.IOException;
 public class DispatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("Work?");
+        Rq rq = new Rq(req, resp);
+
+        MemberController memberController = new MemberController();
+        ArticleController articleController = new ArticleController();
+
+        // getRequestURI는
+        // http://localhost:8081/usr/article/list/free?page=1 에서
+        // /usr/article/list/free 부분만 가져온다.
+        String url = req.getRequestURI();
+
+        switch (url) {
+            case "/usr/article/list/free":
+                articleController.showList(rq);
+                break;
+            case "/usr/member/login":
+                memberController.showLogin(rq);
+                break;
+        }
     }
 }
 
